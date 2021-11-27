17b4b6beb9f8a32829304a9714f04c9e41c4ebfa
Kristoffer Vassbø <kristoffervassbo@gmail.com>
2021-11-27 17:43:31 +0100

    ✏ Updated skills, project view + list & Prettier!

--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -1,90 +1,72 @@
 // random greeting
-const titles = ['Velkommen!', 'Hei.', 'Heisann.', 'Hallo.', 'Halloen.', ':)', ':D'];
+const titles = ["Velkommen!", "Hei.", "Heisann.", "Hallo.", "Halloen.", ":)", ":D"]
 
-var h = new Date().getHours();
-if (h < 10) titles.push('God morgen!');
-else if (h < 18) titles.push('God dag!');
-else titles.push('God kveld!');
+var h = new Date().getHours()
+if (h < 10) titles.push("God morgen!")
+else if (h < 18) titles.push("God dag!")
+else titles.push("God kveld!")
 
-document.querySelector('.top').querySelector('h1').innerHTML = titles[Math.floor(Math.random() * titles.length)];
+document.querySelector(".top").querySelector("h1").innerHTML = titles[Math.floor(Math.random() * titles.length)]
 
 // create track cards
-(function loadTracks() {
-  let seperator = false;
-  console.log(tracks);
+;(function loadTracks() {
+  let seperator = false
+  console.log(tracks)
   for (let name of Object.keys(tracks)) {
-    let track = tracks[name];
-    let elem = document.createElement('div');
-    if (seperator) elem.classList.add('hidden');
+    let track = tracks[name]
+    let elem = document.createElement("div")
+    if (seperator) elem.classList.add("hidden")
 
-    if (name === 'SEPERATOR') {
-      seperator = true;
-      elem.innerHTML = `<span title='Vis flere' class="play" onclick="showMoreMusic(this);" tabindex="0"><span class="cover-preview"></span><span class="material-icons" style="opacity: 1;color: var(--primary);">more_horiz</span></span>`;
-    } else elem.innerHTML = `<span id="${name}" title='Spill av "${name}"' class="play" onclick="playTrack('${name}', this);" tabindex="0">
+    if (name === "SEPERATOR") {
+      seperator = true
+      elem.innerHTML = `<span title='Vis flere' class="play" onclick="showMoreMusic(this);" tabindex="0"><span class="cover-preview"></span><span class="material-icons" style="opacity: 1;color: var(--primary);">more_horiz</span></span>`
+    } else
+      elem.innerHTML = `<span id="${name}" title='Spill av "${name}"' class="play" onclick="playTrack('${name}', this);" tabindex="0">
       <img src="./assets/covers/resized/${name}.jpg" alt="Cover for ${name}" class="cover-preview">
       <span class="material-icons">play_arrow</span><span class="info">${track.duration}</span>
     </span>
-    <span><span>${checkName(name)}</span><p>${getDate(track.uploaded)}</p></span>`;
+    <span><span>${checkName(name)}</span><p>${getDate(track.uploaded)}</p></span>`
 
-    document.querySelector('.tracks').appendChild(elem);
-  };
-  delete tracks.SEPERATOR;
+    document.querySelector(".tracks").appendChild(elem)
+  }
+  delete tracks.SEPERATOR
 
   function checkName(name) {
-    let remix = name.indexOf(' (Vizzber Remix)');
-    if (remix > -1) name = name.slice(0, remix);
-    return name;
+    let remix = name.indexOf(" (Vizzber Remix)")
+    if (remix > -1) name = name.slice(0, remix)
+    return name
   }
   function getDate(date) {
-    date = date.split('/');
-    let month = ['jan.', 'feb.', 'mars', 'apr.', 'mai', 'juni', 'juli', 'aug.', 'sep.', 'okt.', 'nov.', 'des.'][Number(date[1]) - 1];
-    return date[0] + '. ' + month + ' 20' + date[2];
+    date = date.split("/")
+    let month = ["jan.", "feb.", "mars", "apr.", "mai", "juni", "juli", "aug.", "sep.", "okt.", "nov.", "des."][Number(date[1]) - 1]
+    return date[0] + ". " + month + " 20" + date[2]
   }
 })()
 
-
 // scroll -> header
-const sections = document.querySelectorAll('section');
+const sections = document.querySelectorAll("section")
 function changeLinkState() {
-  let i = sections.length;
-  while(--i && window.pageYOffset + 50 < sections[i].offsetTop) {}
+  let i = sections.length
+  while (--i && window.pageYOffset + 50 < sections[i].offsetTop) {}
 
-  document.querySelector('header').querySelector('.active').classList.remove('active');
-  document.querySelector('header').querySelectorAll('a')[i].classList.add('active');
+  document.querySelector("header").querySelector(".active").classList.remove("active")
+  document.querySelector("header").querySelectorAll("a")[i].classList.add("active")
 }
-window.addEventListener('scroll', changeLinkState);
-
+window.addEventListener("scroll", changeLinkState)
 
 // collapsibles
-document.getElementById('age').innerText = Math.abs(new Date(Date.now() - new Date("November 05, 2002 21:51:00").getTime()).getUTCFullYear() - 1970);
+document.getElementById("age").innerText = Math.abs(new Date(Date.now() - new Date("November 05, 2002 21:51:00").getTime()).getUTCFullYear() - 1970)
+;[...document.getElementsByClassName("collapsible")].forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    e.target.classList.toggle("active")
+    e.target.nextElementSibling.classList.toggle("hidden")
+    document.activeElement.blur()
+  })
+})
 
-[...document.getElementsByClassName('collapsible')].forEach(elem => {
-  elem.addEventListener('click', function() {
-    this.classList.toggle('active');
-    this.nextElementSibling.classList.toggle('hidden');
-    document.activeElement.blur();
-  });
-});
-
-const skills = {
-  HTML: 98,
-  CSS: 85,
-  JavaScript: 75,
-  MySQL: 70,
-  jQuery: 65,
-  PHP: 60,
-  Java: 35,
-  Linux: 30,
-  React: 25,
-  WordPress: 20,
-  VueJS: 18,
-  NodeJS: 10,
-  Python: 1
-}
-
-let html = '';
-Object.keys(skills).forEach(skill => {
+let html = ""
+Object.keys(skills).forEach((skill) => {
   html += `<span class="projectsItem"><h5>${skill}</h5>
-    <nav class="slider" style="margin: 0;"><span class="base"><span class="progressBar" style="width: ${skills[skill]}%;"></span></span></nav></span>`;
-});
-document.getElementById('skills').innerHTML = html;
+    <nav class="slider" style="margin: 0;"><span class="base"><span class="progressBar" style="width: ${skills[skill]}%;"></span></span></nav></span>`
+})
+document.getElementById("skills").innerHTML = html
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,91 @@
 ///// PROJECTS /////
 
 // sort projects by completeness
-var projectsSorted = {}, tempArray = [];
-for (var n in projects) tempArray.push([n, projects[n].completeness]);
-tempArray.sort((b, a) => {return a[1] - b[1]});
-tempArray.forEach(item => projectsSorted[item[0]] = projects[item[0]]);
+var projectsSorted = {},
+  tempArray = []
+for (var n in projects) tempArray.push([n, projects[n].completeness])
+tempArray.sort((b, a) => a[1] - b[1])
+tempArray.forEach((item) => (projectsSorted[item[0]] = projects[item[0]]))
 
-loadProject(projects, document.getElementById('prosjekter').querySelector('.byCustom'));
-loadProject(projectsSorted, document.getElementById('prosjekter').querySelector('.byProgress'));
+loadProject(projects, document.getElementById("prosjekter").querySelector(".byCustom"))
+loadProject(projectsSorted, document.getElementById("prosjekter").querySelector(".byProgress"))
 
 // create projects list
 function loadProject(object, elem) {
-  var hiddenList = document.createElement('div');
-  hiddenList.classList.add('hiddenList', 'hidden');
-  let seperator = false;
-  Object.keys(object).forEach(name => {
-    let obj = object[name];
-    let item = `<span onclick="openProject('${name}');" style="cursor: pointer;" tabindex="0">${name}</span>`;
-    if (obj.url) item = `<a href="${obj.url}" target="_blank" rel="noopener">${name}</a>`;
+  var hiddenList = document.createElement("div")
+  hiddenList.classList.add("hiddenList", "hidden")
+  let seperator = false
+  Object.keys(object).forEach((name) => {
+    let obj = object[name]
+    let item = `<span onclick="openProject('${name}');" style="cursor: pointer;" tabindex="0">${name}</span>`
+    if (obj.url)
+      item = `<a style="display: flex;align-items: center;" href="${obj.url}" target="_blank" rel="noopener">${name}<svg style="fill: white;padding: 0 5px;height: 1em;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg></a>`
 
-    var newDiv = `<span class="projectsItem"><p>${item}</p>
-    <nav class="slider" title="${obj.completeness}% fullført" style="margin: 0;"><span class="base"><span class="progressBar" style="width: ${obj.completeness}%;"></span></span></nav></span>`;
+    var newDiv = `<span class="projectsItem" onclick="clickChild(this)"><p>${item}</p>
+    <nav class="slider" title="${obj.completeness}% fullført" style="margin: 0;"><span class="base"><span class="progressBar" style="width: ${obj.completeness}%;"></span></span></nav></span>`
 
-    if (name === 'SEPERATOR') {
-      seperator = true;
-      newDiv = '<span style="display: flex;justify-content: center;"><button onclick="showMore(this);" class="outline" style="width: 30%;margin: 10px 0;">Vis flere prosjekter</button></span>';
-      elem.innerHTML += newDiv;
-    } else if (seperator) hiddenList.innerHTML += newDiv;
-    else elem.innerHTML += newDiv;
-  });
-  elem.appendChild(hiddenList);
+    if (name === "SEPERATOR") {
+      seperator = true
+      newDiv =
+        '<span style="display: flex;justify-content: center;"><button onclick="showMore(this);" class="outline" style="width: 30%;margin: 10px 0;">Vis flere prosjekter</button></span>'
+      elem.innerHTML += newDiv
+    } else if (seperator) hiddenList.innerHTML += newDiv
+    else elem.innerHTML += newDiv
+  })
+  elem.appendChild(hiddenList)
 }
-
 
 // project buttons
 
 function showMore(elem) {
-  elem = elem.closest('div');
-  elem.querySelector('.hiddenList').classList.toggle('hidden');
-  elem.querySelector('button').classList.toggle('active');
+  elem = elem.closest("div")
+  elem.querySelector(".hiddenList").classList.toggle("hidden")
+  elem.querySelector("button").classList.toggle("active")
 }
 
 function sortProjects(elem) {
-  if (elem.classList.contains('active')) elem.title = 'Sorter etter fullført';
-  else elem.title = 'Sorter etter relevans';
-  elem.classList.toggle('active');
-  document.getElementById('prosjekter').querySelector('.byCustom').classList.toggle('hidden');
-  document.getElementById('prosjekter').querySelector('.byProgress').classList.toggle('hidden');
+  if (elem.classList.contains("active")) elem.title = "Sorter etter fullført"
+  else elem.title = "Sorter etter relevans"
+  elem.classList.toggle("active")
+  document.getElementById("prosjekter").querySelector(".byCustom").classList.toggle("hidden")
+  document.getElementById("prosjekter").querySelector(".byProgress").classList.toggle("hidden")
 }
 
+function clickChild(e) {
+  e.querySelector("p").children[0].click()
+}
 function openProject(name) {
-  document.getElementById('projectPopup').classList.remove('hidden');
-  document.getElementById('projectPopup').querySelector('h3').innerText = name;
-  document.getElementById('projectPopup').querySelector('p').innerText = projects[name].description || '';
-  document.getElementById('projectPopup').querySelector('iframe').src = 'prosjekter/' + name + '/index.html';
-  document.getElementById('projectPopup').querySelector('iframe').focus();
+  document.getElementById("projectPopup").classList.remove("hidden")
+  document.getElementById("projectPopup").querySelector("h3").innerText = name
+  document.getElementById("projectPopup").querySelector("p").innerText = projects[name].description || ""
+  document.getElementById("projectPopup").querySelector("iframe").src = "prosjekter/" + name + "/index.html"
+  document.getElementById("projectPopup").querySelector("iframe").focus()
 }
 function closePopup() {
-  document.getElementById('projectPopup').classList.add('hidden');
+  document.getElementById("projectPopup").classList.add("hidden")
 }
-document.addEventListener('keydown', e => {
-  if (e.key === 'Escape' && !document.getElementById('projectPopup').classList.contains('hidden')) document.getElementById('projectPopup').classList.add('hidden');
-  else if (e.key === 'Enter' && e.target.getAttribute('tabindex') !== null) e.target.click();
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !document.getElementById("projectPopup").classList.contains("hidden")) document.getElementById("projectPopup").classList.add("hidden")
+  else if (e.key === "Enter" && e.target.getAttribute("tabindex") !== null) e.target.click()
 })
-
 
 ///// MORE /////
 
-
 // light mode
-var r = document.querySelector(':root');
-var theme = false;
+var r = document.querySelector(":root")
+var theme = false
 var lightTheme = {
-  background: '#eff6ff',
-  text: 'black',
-  'background-transparent': 'rgb(255 255 255 / 40%)',
-  'transparent--5': 'rgb(0 0 0 / 5%)',
-  'transparent--10': 'rgb(0 0 0 / 10%)',
-  'transparent--20': 'rgb(0 0 0 / 20%)',
-  'transparent--30': 'rgb(0 0 0 / 30%)'
+  background: "#eff6ff",
+  text: "black",
+  "background-transparent": "rgb(255 255 255 / 40%)",
+  "transparent--5": "rgb(0 0 0 / 5%)",
+  "transparent--10": "rgb(0 0 0 / 10%)",
+  "transparent--20": "rgb(0 0 0 / 20%)",
+  "transparent--30": "rgb(0 0 0 / 30%)",
 }
 
 function changeTheme() {
-  theme = !theme;
-  if (theme) Object.keys(lightTheme).forEach(key => r.style.setProperty('--' + key, lightTheme[key]));
-  else document.documentElement.removeAttribute("style");
+  theme = !theme
+  if (theme) Object.keys(lightTheme).forEach((key) => r.style.setProperty("--" + key, lightTheme[key]))
+  else document.documentElement.removeAttribute("style")
 }
